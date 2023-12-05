c43b245523095c02db956b1a3ee6dd8acb4d88c5
plbaelde <pl.baelde@gmail.com>
2023-12-05 23:11:24 +0100

    pckg up + simplification css

--- src/front/main.tsx
+++ src/front/main.tsx
@@ -2,19 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import './selfthinker-CSS-Playing-Cards/cards.css'
-import "./selfthinker-CSS-Playing-Cards/faces/JC.gif"
-import "./selfthinker-CSS-Playing-Cards/faces/JD.gif"
-import "./selfthinker-CSS-Playing-Cards/faces/JH.gif"
-import "./selfthinker-CSS-Playing-Cards/faces/JS.gif"
-import "./selfthinker-CSS-Playing-Cards/faces/QC.gif"
-import "./selfthinker-CSS-Playing-Cards/faces/QD.gif"
-import "./selfthinker-CSS-Playing-Cards/faces/QH.gif"
-import "./selfthinker-CSS-Playing-Cards/faces/QS.gif"
-import "./selfthinker-CSS-Playing-Cards/faces/KC.gif"
-import "./selfthinker-CSS-Playing-Cards/faces/KD.gif"
-import "./selfthinker-CSS-Playing-Cards/faces/KH.gif"
-import "./selfthinker-CSS-Playing-Cards/faces/KS.gif"
+import './cards.css'
 import "./../../templates/favicon-dark.png"
 import "./../../templates/favicon-light.png"
 import { GameContextProvider } from './hooks/useGame.tsx'
